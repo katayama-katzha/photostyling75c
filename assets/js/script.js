70869bb923772dd7f68d3cc3c0fab9d5857b03e7
TAKAHITO KATAYAMA <katzha.work@gmail.com>
2022-11-11 20:01:10 +0900

    first-commit

---
+++ assets/js/script.js
@@ -0,0 +1,76 @@
+window.addEventListener('load', (event) => {
+  const spinner = document.getElementById('loading');
+  spinner.classList.add('loaded');
+
+});
+
+
+
+jQuery(window).on('load', function() {
+  jQuery("body").removeClass("preload");
+
+});
+
+jQuery(function($){
+  $(function(){
+    if (window.matchMedia('(max-width: 767px)').matches){
+      $('#items .card:nth-child(2n + 1)').attr('data-wow-delay', '0.2s');
+
+    }else{
+
+    }
+   
+    $(window).resize(function() {
+    if (window.matchMedia('(max-width: 767px)').matches){
+      $('#items .card:nth-child(2n + 1)').attr('data-wow-delay', '0.2s');
+
+    }else{
+      $('#items .card:nth-child(3n + 1)').attr('data-wow-delay', '0.2s');
+
+    }
+  
+    
+  
+  
+  });
+  }());
+  $( function() {
+    $( 'a[href^="#"]' ).on( 'click', function(e){
+      var speed = 500;
+      var type = 'swing';
+      var href= $( this ).attr( 'href' );
+      var target = $( href === '#' || href === '' ? 'html' : href );
+      var position = target.offset().top;
+      $( 'html, body' ).animate( { scrollTop:position }, speed, type );
+      e.preventDefault();
+    });
+  });
+
+  $( function() {
+    $( '.burger' ).on( 'click', function(e){
+      $(this).toggleClass('active');
+      $('.right').toggleClass('active');
+      $('.menu').toggleClass('active');
+      e.preventDefault();
+    });
+    $( '.close' ).on( 'click', function(e){
+      $('.burger').removeClass('active');
+      $('.menu').removeClass('active');
+      e.preventDefault();
+    });
+  });
+
+
+  $(function () {
+    $(window).on("scroll", function () {
+      const sliderHeight = 500;
+      if (sliderHeight + 30 < $(this).scrollTop()) {
+        $(".totop").addClass("bottomScroll");
+      } else {
+        $(".totop").removeClass("bottomScroll");
+      }
+    });
+  });
+
+
+});
